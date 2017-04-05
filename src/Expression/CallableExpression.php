369ae7710bbfb0b29fa09d8194c8d01c53f035ad
Jeremiah VALERIE <jeremiah@over-blog.com>
2017-04-05 09:21:02 +0200

    wip

--- src/Expression/CallableExpression.php
+++ src/Expression/CallableExpression.php
@@ -16,15 +16,28 @@ namespace Overblog\GraphQLGenerator\Expression;
  */
 class CallableExpression
 {
+    const PHP_CALLABLE_STATIC_NOTATION_REGEX = '/^(?<function>(?:\\\\?[a-z_\x7f-\xff][a-z0-9_\x7f-\xff]*)+)(?:\:\:(?<method>[a-z_\x7f-\xff][a-z0-9_\x7f-\xff]*))?$/i';
+
     private $expression;
 
     public function __construct($expression)
     {
-        $this->expression = (string) $expression;
+        $this->expression = $this->prepareExpression($expression);
     }
 
     public function __toString()
     {
         return $this->expression;
+    }
+
+    private function prepareExpression($expression)
+    {
+        $expression = trim($expression);
+
+        if (preg_match(self::PHP_CALLABLE_STATIC_NOTATION_REGEX, $expression, $matches)) {
+            $expression = sprintf('\'%s\'', $expression);
+        }
+
+        return $expression;
     }
 }
